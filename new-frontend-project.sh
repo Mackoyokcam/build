c233ba3da6e4e51b04d026a3c983d72a520ccf25
mackoyokcam <mark.hangarter@gmail.com>
2017-10-16 19:28:14 -0700

    removed dev dependencies

--- new-frontend-project.sh
+++ new-frontend-project.sh
@@ -4,7 +4,8 @@ touch README.md src/main.js src/style/main.scss
 curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/.eslintignore
 curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/.eslintrc.json
 curl -o .gitignore https://www.gitignore.io/api/osx%2Cnode%2Clinux%2Cwindows
+curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/webpack.config.js
+curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/.babelrc
 # curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/.travis.yml
 npm init -y
-npm i -D jest eslint nodemon superagent faker aws-sdk-mock
 npm i -S multer body-parser cors morgan dotenv express mongoose bcrypt http-errors jsonwebtoken aws-sdk fs-extra
