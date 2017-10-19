8cd3787bbd9a75bb57a9d7a97d96d1c6f6922df2
mackoyokcam <mark.hangarter@gmail.com>
2017-10-18 17:01:13 -0700

    frontend eslint

--- new-frontend-project.sh
+++ new-frontend-project.sh
@@ -2,11 +2,11 @@
 mkdir -p src src/style
 touch README.md src/main.js src/style/main.scss
 curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/.eslintignore
-curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/.eslintrc.json
+curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/.frontend.eslintrc.json
 curl -o .gitignore https://www.gitignore.io/api/osx%2Cnode%2Clinux%2Cwindows
 curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/webpack.config.js
 curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/.babelrc
 # curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/.travis.yml
 npm init -y
 npm i -S babel-core babel-loader babel-plugin-transform-object-rest-spread babel-preset-env babel-preset-react css-loader extract-text-webpack-plugin \
-html-webpack-plugin node-sass react react-dom resolve-url-loader sass-loader webpack webpack-dev-server eslint-plugin-react
+html-webpack-plugin node-sass react react-dom resolve-url-loader sass-loader webpack webpack-dev-server
