50d16cb8a868cc79c81a7414f49a32a7540ec1a5
mackoyokcam <mark.hangarter@gmail.com>
2017-10-18 21:50:27 -0700

    new new

--- new-frontend-project.sh
+++ new-frontend-project.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-mkdir -p src src/style
+mkdir -p src src/style src/component src/component/app
 touch README.md src/main.js src/style/main.scss
 curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/.eslintignore
-curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/.frontend.eslintrc.json
+curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/.eslintrc.json
 curl -o .gitignore https://www.gitignore.io/api/osx%2Cnode%2Clinux%2Cwindows
 curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/webpack.config.js
 curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/.babelrc
-# curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/.travis.yml
+# curl -O https://raw.githubusercontent.com/Mackoyokcam/build/master/.travis.yml\
+echo build >> .gitignore
 npm init -y
 npm i -S babel-core babel-loader babel-plugin-transform-object-rest-spread babel-preset-env babel-preset-react css-loader extract-text-webpack-plugin \
-html-webpack-plugin node-sass react react-dom resolve-url-loader sass-loader webpack webpack-dev-server superagent
+html-webpack-plugin node-sass react react-router-dom react-dom resolve-url-loader sass-loader webpack webpack-dev-server superagent
